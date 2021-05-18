4ce7f146311a77e6d5694eb5cfa953589e139093
alexbuyan <sashabuyan@yandex.ru>
2021-05-18 12:31:22 +0300

    last task

--- 27-210518/problem/03_concat_separated.h
+++ 27-210518/problem/03_concat_separated.h
@@ -8,11 +8,39 @@
 #include <utility>
 
 namespace cls_27 {
-// TODO: std::string concat_separated(std::string_view sep, .....) { std::stringstream .... }
-template<class ...Args>
-std::string concat_separated(std::string_view sep, const Args &...args) {
+// TODO: std::string concat_separated(std::string_view sep, .....) {
+// std::stringstream .... }
+
+namespace implement {
+void append(bool first, std::stringstream &, std::string_view) {
+}
+
+template <class Arg, class... Args>
+void append(bool first,
+            std::stringstream &s,
+            std::string_view sep,
+            const Arg &arg,
+            const Args &... args) {
+    if (!first)
+        s << sep;
+    s << arg;
+    append(false, s, sep, args...);
+}
+}  // namespace implement
+
+template <class... Args>
+std::string concat_separated(std::string_view sep, const Args &... args) {
     std::stringstream s;
+    implement::append(true, s, sep, args...);
     return s.str();
+}
+
+template <class... Args>
+std::string concat_separated(std::string_view sep, const std::tuple<Args...> &args) {
+    return std::apply(
+        static_cast<std::string (*)(std::string_view, const Args &...)>(
+            concat_separated),
+        std::tuple_cat(std::tuple{sep}, args));
 }
 }  // namespace cls_27
 
