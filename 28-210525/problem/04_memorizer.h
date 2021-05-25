d7b67c9634c6e2af03876cfda4907183457a0d44
alexbuyan <sashabuyan@yandex.ru>
2021-05-25 12:18:43 +0300

    solution

--- 28-210525/problem/04_memorizer.h
+++ 28-210525/problem/04_memorizer.h
@@ -7,28 +7,66 @@
 #include <vector>
 
 namespace cls_28 {
-template<typename R, typename ...Args>
+template <typename R, typename... Args>
 struct CallLog {
     R result;
     std::tuple<Args...> arguments;
     // TODO: CallLog(result, arguments....) consturctor
+
+    CallLog(R result_, Args... args)
+        : result(std::move(result_)), arguments(std::move(args)...) {
+    }
+
+    CallLog(R result_, std::tuple<Args...> arguments_)
+        : result(std::move(result_)), arguments(std::move(arguments_)) {
+    }
+};
+
+template <typename... Args>
+struct CallLog<void, Args...> {
+    std::tuple<Args...> arguments;
+    CallLog(Args... args) : arguments(std::move(args)...) {
+    }
+    CallLog(std::tuple<Args...> arguments_) : arguments(std::move(arguments_)) {
+    }
 };
 
 namespace impl {
-    template<typename Fn>
-    struct function_traits {};
-    // TODO: specialize for function pointers
+template <typename Fn>
+struct function_traits {};
+// TODO: specialize for function pointers
+
+template <typename R, typename... Args>
+struct function_traits<R (*)(Args...)> {
+    using call_result_type =
+        CallLog<std::remove_reference_t<R>, std::remove_reference_t<Args>...>;
+};
+
 }  // namespace impl
 
-template<typename Fn, typename C>
+template <typename Fn,
+          typename C = typename impl::function_traits<Fn>::call_result_type>
 struct Memorizer {
     Fn fn;
 
     std::vector<C> calls;
 
-    Memorizer(Fn fn_) : fn(std::move(fn_)) {}
+    Memorizer(Fn fn_) : fn(std::move(fn_)) {
+    }
 
     // TODO: overload operator() for arbitrary arguments
+    template <typename... Args>
+    decltype(auto) operator()(Args &&... args) {
+        std::tuple<std::remove_reference_t<Args>...> arguments(args...);
+        if constexpr (std::is_same_v<void, decltype(fn(args...))>) {
+            fn(args...);
+            calls.emplace_back(std::move(arguments));
+        } else {
+            decltype(auto) result = fn(args...);
+            calls.emplace_back(result, std::move(arguments));
+            return result;
+        }
+    }
 };
 }  // namespace cls_28
 
